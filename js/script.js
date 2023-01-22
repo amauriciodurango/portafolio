b264fc693e51dcf4b7a2efca29421ae0cf8dbd6d
amauricio <amauriciodurango@gmail.com>
2023-01-22 15:24:29 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function encriptar() {
+
+    
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/e/img, "enter");  
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober"); 
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes"); 
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai"); 
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat"); 
+
+    document.getElementById("imgDer").style.display= "none";
+    document.getElementById("textoDerecho").style.display = "none";
+    document.getElementById("textoDerecho2").style.display = "none";
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+    document.getElementById("botonCopiar").style.display="show";
+    document.getElementById("botonCopiar").style.display="inherit";
+
+}
+
+function desencriptar() {
+
+    
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/enter/img, "e");  
+    var textoEncriptado = textoEncriptado.replace(/ober/img, "o"); 
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i"); 
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a"); 
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u"); 
+
+    document.getElementById("imgDer").style.display= "none";
+    document.getElementById("textoDerecho").style.display = "none";
+    document.getElementById("textoDerecho2").style.display = "none";
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+    document.getElementById("botonCopiar").style.display="show";
+    document.getElementById("botonCopiar").style.display="inherit";
+
+}
+
+function copiar() {
+    var contenido = document.querySelector("#textoDesencriptado");
+    contenido.select();
+    document.execCommand("Copy");
+
+}
